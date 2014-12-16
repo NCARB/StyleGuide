ed9f0d394c0139f40e299e17e0467feae7de60b0
Equan Simmonds <esimmonds@ncarb.org>
2014-12-16 11:51:45 -0500

    Add rest of the routes

--- js/app.js
+++ js/app.js
@@ -1,13 +1,32 @@
 angular.module('designSystem', ['ngRoute'])
-.config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: '../patterns/introduction.html'        
-      })
-      .when('/color', {
-        templateUrl: '../patterns/introduction.html'        
-      });
+	.config(['$routeProvider', '$locationProvider',
+		function($routeProvider, $locationProvider) {
+			$routeProvider
+				.when('/', {
+					templateUrl: '../patterns/introduction.html'
+				})
+				.when('/layout', {
+					templateUrl: '../patterns/layout.html'
+				})
+				.when('/color', {
+					templateUrl: '../patterns/color.html'
+				})
+				.when('/typography', {
+					templateUrl: '../patterns/typography.html'
+				})
+				.when('/iconography', {
+					templateUrl: '../patterns/iconography.html'
+				})
+				.when('/formElements', {
+					templateUrl: '../patterns/form_elements.html'
+				})
+				.when('/navigation', {
+					templateUrl: '../patterns/navigation.html'
+				})
+				.when('/content', {
+					templateUrl: '../patterns/content.html'
+				});
 
-    $locationProvider.html5Mode(true);
-}])
+			$locationProvider.html5Mode(true);
+		}
+	]);
