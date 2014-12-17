632ebe008d3b547f49510aee611bbf200d13e88b
Equan Simmonds <esimmonds@ncarb.org>
2014-12-16 19:56:37 -0500

    Remove base Url

--- js/app.js
+++ js/app.js
@@ -7,35 +7,35 @@ angular.module('designSystem', ['ui.router'])
 			$stateProvider
 				.state('introduction', {
 					url: '/',
-					templateUrl: '../patterns/introduction.html'
+					templateUrl: 'patterns/introduction.html'
 				})
 				.state('layout', {
 					url: '/layout',
-					templateUrl: '../patterns/layout.html'
+					templateUrl: 'patterns/layout.html'
 				})
 				.state('color', {
 					url: '/color',
-					templateUrl: '../patterns/color.html'
+					templateUrl: 'patterns/color.html'
 				})
 				.state('typography', {
 					url: '/typography',
-					templateUrl: '../patterns/typography.html'
+					templateUrl: 'patterns/typography.html'
 				})
 				.state('iconography', {
 					url: '/iconography',
-					templateUrl: '../patterns/iconography.html'
+					templateUrl: 'patterns/iconography.html'
 				})
 				.state('formElements', {
 					url: '/formElements',
-					templateUrl: '../patterns/form_elements.html'
+					templateUrl: 'patterns/form_elements.html'
 				})
 				.state('navigation', {
 					url: '/navigation',
-					templateUrl: '../patterns/navigation.html'
+					templateUrl: 'patterns/navigation.html'
 				})
 				.state('content', {
 					url: '/content',
-					templateUrl: '../patterns/content.html'
+					templateUrl: 'patterns/content.html'
 				});
 		}
 	]);
